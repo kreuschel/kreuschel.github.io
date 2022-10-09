ca222f131641d50783479a119d43473bd9b14d25
William <5170124+wbreuschel@users.noreply.github.com>
2022-10-09 11:36:11 -0400

    updated background image

--- pages/index.js
+++ pages/index.js
@@ -12,16 +12,16 @@ export default function Home() {
       <main className={styles.main}>
   <div class="mb-32 text-gray-800 text-center lg:text-left">
 
-    <div class="md:px-6 py-12 md:px-12">
+    <div className={`${styles.heroBackground} md:px-6 py-12 md:px-12`}>
       <div class="max-w-4xl container mx-auto ">
-        <div class="grid lg:grid-cols-2 flex items-end">
+        <div class="grid lg:grid-cols-2 flex items-center">
           <div class="md:mt-12 lg:mt-0 mb-12 lg:mb-0">
             <div className={styles.heroCard}>
               <h1 className={styles.title}><div className="sm:inline-block">Kristin</div><div className="lg:text-right">Reuschel</div><span className={styles.subtitle}>Curriculum Designer | Program Manager</span></h1>
             </div>
           </div>
           <div class="md:mb-12 lg:mb-0">
-          <img class="w-full rounded-lg shadow-lg object-center" src="assets/portrait.svg" alt="vector art silhoutte of my head in profile"/>
+          <img class="w-full rounded-lg shadow-lg object-center fill-white" src="assets/portrait.svg" alt="vector art silhoutte of my head in profile"/>
           </div>
         </div>
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
+        {/* <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
@@ -39,7 +39,7 @@ export default function Home() {
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
-        </a>
+        </a> */}
       </footer>
     </div>
   )
